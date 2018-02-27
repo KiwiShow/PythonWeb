7994219430b5264d1c6f91bbc08a8cc97a60ab4e
Kiwi <cw910217@gmail.com>
2018-02-27 11:06:28 +0800

    +使log能够输出到文本

--- server_normal/utils.py
+++ server_normal/utils.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 
 # 对print函数增强，增加了一个时间str
+# 再次增强log函数，增加了输出到文本的功能
 def log(*args, **kwargs):
     format_time = '%Y/%m/%d %H:%M:%S'
     # localtime()默认的是参数是time.time()
@@ -10,7 +11,9 @@ def log(*args, **kwargs):
     dt = time.strftime(format_time, value)
     # 对于No Newline at End of File的问题
     # set "Ensure line feed at file end on Save" under "Editor."
-    print(dt, *args, **kwargs)
+    with open('log.txt', 'a', encoding='utf-8') as f:
+        # 不要用f=open() 和 f.close() 的组合，容易忘写 f.close()
+        print(dt, *args, **kwargs, file=f)
 
 
 # 可以用datetime模块试试
