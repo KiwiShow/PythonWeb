320554a4982e322fd0e4030563eebda9a144bf48
Kiwi <cw910217@gmail.com>
2018-02-28 17:25:34 +0800

    +对User类进行改造密码加盐，相应的login和register逻辑修改

--- server_normal/models/user.py
+++ server_normal/models/user.py
@@ -1,5 +1,6 @@
 from models import Model
 from .todo import Todo
+import hashlib
 
 # 继承了 Model
 # 所以可以直接 save load
@@ -11,6 +12,19 @@ class User(Model):
         self.note = form.get('note', '')
         self.role = int(form.get('role', 10))
 
+    def salted_password(self, password, salt='less_is_more!'):
+        def sha256(str):
+            return hashlib.sha256(str.encode('utf-8')).hexdigest()
+        hash1 = sha256(password)
+        hash2 = sha256(hash1 + salt)
+        return hash2
+
+    # 不加salt的password
+    def hashed_password(self, pwd):
+        p = pwd.encode('utf-8')
+        s = hashlib.sha3_256(p)
+        return s.hexdigest()
+
     def validate_login(self):
         # us = User.all()
         # for u in us:
@@ -19,11 +33,17 @@ class User(Model):
         # return False
         # 更加简洁
         user = User.find_by(username=self.username)
-        return user is not None and user.password == self.password
+        return user is not None and user.password == self.salted_password(self.password)
 
     def validate_register(self):
         # 简单验证用户名或者密码长度必须大于2
-        return len(self.username) > 2 and len(self.password) > 2
+        if len(self.username) > 2 and len(self.password) > 2:
+            self.password = self.salted_password(self.password)  # 加盐
+            if User.find_by(username=self.username) is None:
+                self.save()
+                return True
+        return False
+
 
     # 增加一个获取该user全部todo的函数
     def todos(self):
