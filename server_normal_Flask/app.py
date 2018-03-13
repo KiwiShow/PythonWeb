3186c79b8018a518fae1698796ab1c88c651682e
Kiwi <cw910217@gmail.com>
2018-03-13 14:50:15 +0800

    +完成传统tweet和comment的改造

--- server_normal_Flask/app.py
+++ server_normal_Flask/app.py
@@ -4,6 +4,10 @@ from routes.routes_user import main as user_routes
 from routes.routes_static import main as static_routes
 from routes.routes_todo import main as todo_routes
 from routes.routes_todo_ajax import main as todo_routes_ajax
+from routes.routes_tweet import main as tweet_routes
+# from routes.routes_tweet_ajax import main as tweet_routes_ajax
+from routes.routes_comment import main as comment_routes
+
 
 # 为了符合WSGI，将应用包装成一个app，可以被WSGI服务端通过application(env, response)调用
 app = Flask(__name__)
@@ -16,6 +20,10 @@ app.register_blueprint(user_routes)
 app.register_blueprint(static_routes)
 app.register_blueprint(todo_routes, url_prefix='/todo')
 app.register_blueprint(todo_routes_ajax, url_prefix='/ajax/todo')
+app.register_blueprint(tweet_routes, url_prefix='/tweet')
+# app.register_blueprint(tweet_routes_ajax, url_prefix='/ajax/tweet')
+app.register_blueprint(comment_routes, url_prefix='/comment')
+
 
 # 运行
 if __name__ == '__main__':
---
+++ server_normal_Flask/routes/routes_comment.py
@@ -0,0 +1,66 @@
+from models.user import User
+from models.tweet import Tweet
+from models.comment import Comment
+
+from utils import log
+from flask import (
+    request,
+    Blueprint,
+    render_template,
+    redirect,
+    url_for,
+    session,
+    make_response,
+)
+
+from routes.routes_user import current_user
+from routes.routes_todo import login_required
+
+main = Blueprint('comment', __name__)
+
+
+@main.route('/add', methods=['POST'])
+def add():
+    """
+    添加comment
+    :return: 返回index页面
+    """
+    user = current_user()
+    form = request.form
+    c = Comment.new(form, user_id=user.id, user_name=user.username)
+    # c.save()
+    # uid = c.tweet().user().id
+    return redirect(url_for('tweet.index'))
+
+
+@main.route('/delete', methods=['GET'])
+def delete():
+    u = current_user()
+    comment_id = request.args.get('id', -1)
+    comment_id = int(comment_id)
+    c = Comment.find(comment_id)
+    if u.id == c.user_id:
+        c.remove(comment_id)
+    return redirect(url_for('tweet.index'))
+
+
+@main.route('/edit', methods=['GET'])
+def edit():
+    u = current_user()
+    comment_id = int(request.args.get('id', -1))
+    c = Comment.find(comment_id)
+    if u.id == c.user_id:
+        body = render_template('comment_edit.html',
+                        comment_id=c.id,
+                        comment_content=c.content)
+        return make_response(body)
+    return redirect(url_for('tweet.index'))
+
+
+@main.route('/update', methods=['POST'])
+def update():
+    form = request.form
+    Comment.check_id(form)
+    newTweet = Comment.update(form)
+    # redirect有必要加query吗
+    return redirect(url_for('tweet.index'))
