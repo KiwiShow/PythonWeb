2f1dfa7e783eb36b3e750ddedcca2315dfb70873
Kiwi <cw910217@gmail.com>
2018-03-13 11:15:14 +0800

    +对每个路由函数有标注

--- server_normal_Flask/routes/routes_user.py
+++ server_normal_Flask/routes/routes_user.py
@@ -49,6 +49,12 @@ def index():
 
 @main.route('/login', methods=['POST', 'GET'])
 def login():
+    """
+    允许GET是因为在index页面转到login页面需要
+    POST是因为在login页面输入账号密码点击login按钮需要
+    主要的bug是在index和login页面都都是同一个路由函数
+    :return:无论如何都会显示login页面
+    """
     form = request.form
     u = current_user()
     # ImmutableMultiDict([])是什么鬼？
@@ -103,6 +109,10 @@ def login():
 
 @main.route('/out')
 def out():
+    """
+    在session中删除当前登录的user_id
+    :return: 返回login页面
+    """
     session.pop('user_id')
     return redirect(url_for('.login'))
 
@@ -125,12 +135,21 @@ def out():
 
 @main.route('/register', methods=['POST', 'GET'])
 def register():
+    """
+    允许GET是因为在地址栏输入地址转到register页面需要
+    POST是因为在register页面输入账号密码点击register按钮需要
+    主要的bug是转到register页面和register页面都都是同一个路由函数
+    :return: 返回register页面，并显示所有用户信息
+    """
+    result = ''
     form = request.form
-    if User.validate_register(form):
-        result = '注册成功<br> <pre>{}</pre>'.format(User.all())
-    else:
-        result = '用户名或者密码长度必须大于2或者用户名已注册'
-    body = render_template('register.html', result=result)
+    if request.method == 'POST':
+        if User.validate_register(form):
+            # html元素失效
+            result = '注册成功'
+        else:
+            result = '用户名或者密码长度必须大于2或者用户名已注册'
+    body = render_template('register.html', result=result, users=User.all())
     return make_response(body)
 
 
@@ -158,10 +177,17 @@ def register():
 
 @main.route('/messages', methods=['POST', 'GET'])
 def message():
+    """
+    允许GET是因为在地址栏输入地址转到messages页面需要
+    POST是因为在messages页面内容点击"提交"按钮需要
+    主要的bug是转到message页面和message页面都都是同一个路由函数
+    :return: 返回message页面，并显示所有用户信息
+    """
     form = request.form
     msg = Message.new(form)
-    msgs = '<br>'.join([str(m) for m in Message.all()])
-    body = render_template('html_basic.html', messages=msgs)
+    # html元素失效
+    # msgs = '<br>'.join([str(m) for m in Message.all()])
+    body = render_template('html_basic.html', messages=Message.all())
     return make_response(body)
 
 
@@ -196,7 +222,14 @@ def message():
 
 @main.route('/profile', methods=['GET'])
 def profile():
+    """
+    显示已登录用户的信息，木有模板
+    :return: 返回含有已登录用户的信息
+    """
     u = current_user()
+    # html元素有效有效有效有效有效有效有效有效有效有效有效
+    # 似乎render_template函数里面参数赋值的时候有过滤？
+    # 在render_template中直接文本替代是不行的，只会当成字符串
     body = '<h1>id:{} ' \
            'username: {} ' \
            'note: {}</h1>'.format(u.id,
@@ -217,6 +250,10 @@ def profile():
 
 @main.route('/admin/users', methods=['GET'])
 def admin():
+    """
+    只有用户id为1的用户有权限
+    :return: 返回所有用户的信息
+    """
     u = current_user()
     if u.id != 1:
         return redirect(url_for('.login'))
@@ -240,6 +277,10 @@ def admin():
 
 @main.route('/admin/user/update', methods=['POST'])
 def admin_update():
+    """
+    只有用户id为1的用户有权限，输入需要修改的id和password
+    :return: 返回修改过的所有用户的信息
+    """
     u = current_user()
     if u.id != 1:
         return redirect(url_for('.login'))
