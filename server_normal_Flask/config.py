61ac53984a68c9f3632a2b2c2ec01af3b0b631be
Kiwi <cw910217@gmail.com>
2018-03-18 23:56:45 +0800

    +优化

--- server_normal_Flask/config.py
+++ server_normal_Flask/config.py
@@ -3,4 +3,4 @@ import os.path
 image_file_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'image')
 accept_image_file_type = ['jpg', 'gif', 'png']
 
-print(image_file_dir)
+# print(image_file_dir)
