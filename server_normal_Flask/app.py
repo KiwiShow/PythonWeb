3d4c1ce500791eb69c30ae4e7192a2ff7a64fa35
Kiwi <cw910217@gmail.com>
2018-03-20 12:07:13 +0800

    +优化app.py，所以代码都用函数包起来

--- server_normal_Flask/app.py
+++ server_normal_Flask/app.py
@@ -14,27 +14,33 @@ from utils import log
 from config import  secret_key
 
 
-# 为了符合WSGI，将应用包装成一个app，可以被WSGI服务端通过application(env, response)调用
-app = Flask(__name__)
+def configured_app():
+    # 为了符合WSGI，将应用包装成一个app，可以被WSGI服务端通过application(env, response)调用
+    app = Flask(__name__)
 
-# 设置secret_key用来对称加密session
-# 好处是不同的人用不同的secret_key，隔离开发和服务器环境，保证安全
-app.secret_key = secret_key
-# app.secret_key = 'Be the greatest，or nothing'
+    # 设置secret_key用来对称加密session
+    # 好处是不同的人用不同的secret_key，隔离开发和服务器环境，保证安全
+    app.secret_key = secret_key
+    # app.secret_key = 'Be the greatest，or nothing'
+    register_routes(app)
+    return app
 
-# 模块化路由的功能由 蓝图（Blueprints）提供
-app.register_blueprint(user_routes)
-app.register_blueprint(static_routes)
-app.register_blueprint(todo_routes, url_prefix='/todo')
-app.register_blueprint(todo_routes_ajax, url_prefix='/ajax/todo')
-app.register_blueprint(tweet_routes, url_prefix='/tweet')
-app.register_blueprint(tweet_routes_ajax, url_prefix='/ajax/tweet')
-app.register_blueprint(comment_routes, url_prefix='/comment')
-app.register_blueprint(comment_routes_ajax, url_prefix='/ajax/comment')
+
+def register_routes(app):
+    # 模块化路由的功能由 蓝图（Blueprints）提供
+    app.register_blueprint(user_routes)
+    app.register_blueprint(static_routes)
+    app.register_blueprint(todo_routes, url_prefix='/todo')
+    app.register_blueprint(todo_routes_ajax, url_prefix='/ajax/todo')
+    app.register_blueprint(tweet_routes, url_prefix='/tweet')
+    app.register_blueprint(tweet_routes_ajax, url_prefix='/ajax/tweet')
+    app.register_blueprint(comment_routes, url_prefix='/comment')
+    app.register_blueprint(comment_routes_ajax, url_prefix='/ajax/comment')
 
 
 # 运行
 if __name__ == '__main__':
+    app = configured_app()
     config = dict(
         debug=True,
         host='0.0.0.0',
