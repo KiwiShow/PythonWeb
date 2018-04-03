51ef9941bd0ce538a0379fad17b3f10c5f24654f
Kiwi <cw910217@gmail.com>
2018-04-03 23:01:01 +0800

    +完成comment路由简化

--- server_normal_Flask_beautiful/routes/routes_comment.py
+++ server_normal_Flask_beautiful/routes/routes_comment.py
@@ -31,47 +31,38 @@ def add():
     :return: 返回index页面
     """
     user = current_user()
-    token = request.args.get('token')
-    if Comment.check_token(token, gg.csrf_tokens):
+    if Comment.check_token():
         form = request.form
         c = Comment.new(form, user_id=user.id, user_name=user.username)
-        # c.save()
-        # uid = c.tweet().user().id
-        return redirect(url_for('tweet.detail', tweet_id=c.tweet_id, token=token))
+        return redirect(url_for('tweet.detail', tweet_id=c.tweet_id, token=gg.token))
 
 
 @main.route('/delete/<int:comment_id>', methods=['GET'])
 @login_required
 def delete(comment_id):
-    # comment_id = request.args.get('id', -1)
-    # comment_id = int(comment_id)
-    token = request.args.get('token')
-    if Comment.check_token(token, gg.csrf_tokens):
+    if Comment.check_token():
         c = Comment.find(comment_id)
         Comment.check_id(id=comment_id)
         c.remove(comment_id)
-        return redirect(url_for('tweet.detail', tweet_id=c.tweet_id, token=token))
+        return redirect(url_for('tweet.detail', tweet_id=c.tweet_id, token=gg.token))
 
 
 @main.route('/edit/<int:comment_id>', methods=['GET'])
 @login_required
 def edit(comment_id):
     user = current_user()
-    # comment_id = int(request.args.get('id', -1))
-    token = request.args.get('token')
-    if Comment.check_token(token, gg.csrf_tokens):
+    if Comment.check_token():
         c = Comment.find(comment_id)
         Comment.check_id(id=comment_id)
-        return render_template('tweet/comment_edit.html', c=c, token=token, user=user)
+        return render_template('tweet/comment_edit.html', c=c, token=gg.token, user=user)
 
 
 @main.route('/update', methods=['POST'])
 @login_required
 def update():
-    token = request.args.get('token')
-    if Comment.check_token(token, gg.csrf_tokens):
+    if Comment.check_token():
         form = request.form
         Comment.check_id(form)
         newComment = Comment.update(form)
         # redirect有必要加query吗
-        return redirect(url_for('tweet.detail', tweet_id=newComment.tweet_id, token=token))
+        return redirect(url_for('tweet.detail', tweet_id=newComment.tweet_id, token=gg.token))
