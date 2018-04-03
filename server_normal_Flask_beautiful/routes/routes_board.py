acc51d1481d6139f51e2b2493af37927983f3285
Kiwi <cw910217@gmail.com>
2018-04-03 23:02:44 +0800

    +完成board路由简化

--- server_normal_Flask_beautiful/routes/routes_board.py
+++ server_normal_Flask_beautiful/routes/routes_board.py
@@ -25,22 +25,20 @@ main = Blueprint('board', __name__)
 @main.route('/add', methods=['POST'])
 @login_required
 def add():
-    token = request.args.get('token')
-    if Board.check_token(token, gg.csrf_tokens):
+    if Board.check_token():
         User.check_admin()
         form = request.form
         b = Board.new(form)
-        return redirect(url_for('user.admin', token=token))
+        return redirect(url_for('user.admin', token=gg.token))
 
 
 @main.route('/delete/<int:board_id>')
 @login_required
 def delete(board_id):
-    token = request.args.get('token')
-    if Board.check_token(token, gg.csrf_tokens):
+    if Board.check_token():
         User.check_admin()
         Board.remove(board_id)
-        return redirect(url_for('user.admin', token=token))
+        return redirect(url_for('user.admin', token=gg.token))
 
 
 @main.route('/edit/<int:board_id>', methods=['GET'])
@@ -51,20 +49,18 @@ def edit(board_id):
     :return: 返回修改过的所有用户的信息
     """
     user = current_user()
-    token = request.args.get('token')
-    if User.check_token(token, gg.csrf_tokens):
+    if Board.check_token():
         User.check_admin()
         b = Board.find(board_id)
-        return render_template('board/board_edit.html', token=token, user=user, b=b)
+        return render_template('board/board_edit.html', token=gg.token, user=user, b=b)
 
 
 @main.route('/update', methods=['POST'])
 @login_required
 def update():
-    token = request.args.get('token')
-    if Board.check_token(token, gg.csrf_tokens):
+    if Board.check_token():
         User.check_admin()
         form = request.form
         Board.update(form)
         # redirect有必要加query吗
-        return redirect(url_for('user.admin', token=token))
+        return redirect(url_for('user.admin', token=gg.token))
