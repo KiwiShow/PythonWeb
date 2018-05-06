ed7956edf4e5b14ab7401a9a47a3cbe04d4d631e
Kiwi <cw910217@gmail.com>
2018-05-06 23:01:19 +0800

    + wsgi 走的是 gunicorn.config.py 里面设置

--- server_normal_Flask_beautiful/wsgi.py
+++ server_normal_Flask_beautiful/wsgi.py
@@ -11,7 +11,8 @@ sys.path.insert(0, abspath(dirname(__file__)))
 
 # 这里变了骚年
 # wsgi 与 manage 分别是 生产环境(VPS) 和 开发环境 的 启动项， 都是输入  configured_app
-
+# 还有问题： host port 设置了吗？
+# wsgi 走的是 gunicorn.config.py 里面设置
 application = configured_app(os.getenv('FLASK_CONFIG') or 'default')
 
 
